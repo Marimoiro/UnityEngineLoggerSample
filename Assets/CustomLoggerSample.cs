81e3578cd43d9d1320e9108b8e5612d3a1029fbc
Marimoiro <marimoiro.git@gmail.com>
2016-01-06 01:34:11 +0900

    作成

---
+++ Assets/CustomLoggerSample.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Object = UnityEngine.Object;
+
+public class CustomLoggerSample : MonoBehaviour
+{
+
+    ILogger logger;
+    void Awake()
+    {
+        Debug.logger.logEnabled = false;
+        logger = new Logger(new FaceLogHandler());
+    }
+
+    public void OnClick()
+    {
+        logger.Log("顔文字ロガー！");
+    }
+
+    public void OnError()
+    {
+        logger.LogException(new Exception("エラー起こしちゃった"));
+    }
+}
+
+public class FaceLogHandler : ILogHandler
+{
+    public void LogFormat(LogType logType, Object context, string format, params object[] args)
+    {
+        Debug.logger.LogFormat(logType, "(・∀・)" + context, format, args);
+    }
+
+    public void LogException(Exception exception, Object context)
+    {
+        Debug.logger.LogException(new Exception("(´・ω・｀)", exception), context);
+    }
+}
---
+++ Assets/DefaultLoggerSample.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Object = UnityEngine.Object;
+
+public class DefaultLoggerSample : MonoBehaviour
+{
+
+    ILogger logger;
+    void Awake()
+    {
+        logger = new Logger(Debug.logger);
+    }
+
+    public void OnClick()
+    {
+        logger.Log("Default Loggerに流してみる");   
+    }
+}
